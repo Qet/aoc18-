02598201efa21fe9e0946fbbabbb60b0a7f345b6
Rhett Winston <rhettius@gmail.com>
2018-12-06 16:15:27 +1100

    finished day1, part 2.

--- 1/find_freq.py
+++ 1/find_freq.py
@@ -1,16 +1,25 @@
 # AoC day 1, puzzle 2
 
-with open('input') as fh:
+def get_input():
+    out = []
+    with open('input') as fh:
+        for line in fh:
+            out.append(int(line))
+    return out
+    
+def calc_dupe_freq():
+    data = get_input()
+    frequencies = set()
+    frequencies.add(0)
     sum = 0
-    s = set()
-    for line in fh:
-        sum += int(line)
-        if sum in s:
-            print "Frequency {} detected twice! Stopping.".format(sum);
-            break
-        s.add(sum)
+    loops = 0
+    while True:
+        loops += 1
+        for d in data:
+            sum += d
+            if sum in frequencies:
+                return sum, loops # Found a duplicate frequency level
+            frequencies.add(sum)
 
-    print s
-        
-
-
+answer, loops = calc_dupe_freq()
+print "Duplicate frequency level: {}. Loops: {}".format(answer, loops)
