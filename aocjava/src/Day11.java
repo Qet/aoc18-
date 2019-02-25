ff2bccd6d8d279697572a83a9acba390a8adc649
Rhett Winston <rhettius@gmail.com>
2019-02-26 07:34:50 +1100

    day11 part1 done. working on part 2

--- aocjava/src/Day11.java
+++ aocjava/src/Day11.java
@@ -26,8 +26,7 @@ class Grid{
         return powerLevel;
     }
 
-    public int fuelCellPower(int x, int y){
-        final int subGridDimensions = 3;
+    public int fuelCellPower(int x, int y, int subGridDimensions){
 
         if (x < 1 || y < 1 ||
                 (x + subGridDimensions) >= dimensions ||
@@ -36,7 +35,7 @@ class Grid{
 
         int sum = 0;
         for(int i=x; i < x + subGridDimensions ; i++){
-            for(int j=x; j < x + subGridDimensions ; j++) {
+            for(int j=y; j < y + subGridDimensions ; j++) {
                 sum += theGrid[i][j];
             }
         }
@@ -50,12 +49,13 @@ class Grid{
 }
 
 class GridResult{
-    public GridResult(int x, int y, int val) {
+    public GridResult(int x, int y, int val, int size) {
         this.x = x;
         this.y = y;
         this.val = val;
+        this.size = size;
     }
-
+    public int size;
     public int x;
     public int y;
     public int val;
@@ -64,18 +64,18 @@ class GridResult{
 
 
 public class Day11 {
-    public static void main(String[] args) {
 
+    public static void doPart1(){
         final int dimensions = 300;
-        final int serial = 18;
+        final int serial = 2866;
         Grid g = new Grid(serial, dimensions);
 
-        GridResult bestResult = new GridResult(0,0,0);
+        GridResult bestResult = new GridResult(0,0,0, 0);
         for(int i=1; i <= dimensions; i++){
             for(int j=1;j <= dimensions; j++){
-                int curPower = g.fuelCellPower(i, j);
+                int curPower = g.fuelCellPower(i, j, 3);
                 if (curPower > bestResult.val){
-                    bestResult = new GridResult(i, j, curPower);
+                    bestResult = new GridResult(i, j, curPower, 3);
                 }
             }
         }
@@ -83,6 +83,36 @@ public class Day11 {
         System.out.println("[Part 1]: Most powerful grid at: (" +
                 bestResult.x + ", " + bestResult.y + ")" +
                 " Power: " + bestResult.val);
+
+    }
+
+    public static void doPart2(){
+        final int dimensions = 300;
+        final int serial = 2866;
+        Grid g = new Grid(serial, dimensions);
+
+        GridResult bestResult = new GridResult(0,0,0, 0);
+        for(int sz=1; sz <= dimensions; sz++) {
+            System.out.println("Processing size: " + sz);
+            for (int i = 1; i <= dimensions - sz + 1; i++) {
+                for (int j = 1; j <= dimensions - sz + 1; j++) {
+                    int curPower = g.fuelCellPower(i, j, sz);
+                    if (curPower > bestResult.val) {
+                        bestResult = new GridResult(i, j, curPower, sz);
+                    }
+                }
+            }
+        }
+
+        System.out.println("[Part 2]: Most powerful grid at: (" +
+                bestResult.x + ", " + bestResult.y + ")" +
+                " Power: " + bestResult.val +
+                " Size: " + bestResult.size);
+
+    }
+
+    public static void main(String[] args) {
+        doPart2();
 
     }
 }
--- aocjava/src/Day11Testg.groovy
+++ aocjava/src/Day11Testg.groovy
@@ -7,6 +7,10 @@ class Day11Testg extends GroovyTestCase{
     void testCell(){
         Grid g = new Grid(18, 300)
         assertEquals(29, g.fuelCellPower(33, 45))
+
+        g = new Grid(42, 300)
+        assertEquals(30, g.fuelCellPower(21, 61))
+
     }
 
 }
